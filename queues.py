4c03bbe2131139151879295a7705cf2a44db527c
Paulie-Aditya <paul.aditya304@gmail.com>
2023-07-27 07:17:35 +0530

    queues using ll

---
+++ queues.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+
+
+class Queue:
+
+    def __init__(self):
+        self.front = None
+        self.rear = None
+    
+    def enqueue(self,value):
+        new_node = Node(value)
+
+        if self.rear == None:
+            self.front = new_node
+            self.rear = self.front
+        else:
+            #inserting at tail
+            self.rear.next = new_node
+            self.rear = new_node
+    
+    def dequeue(self):
+        if self.front == None:
+            return 'Empty Queue'
+        
+        self.front = self.front.next
+    
+    def traverse(self):
+        temp = self.front
+
+        while temp:
+            print(temp.data, end =" ")
+            temp = temp.next
+        print("\n")
+
+
